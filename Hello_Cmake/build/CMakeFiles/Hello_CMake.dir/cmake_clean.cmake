6a64c1ec0e5722b4f76f5033a72abee36747d4a8
Spectre <66801724+Spectre-1@users.noreply.github.com>
2023-04-24 17:21:56 -0700

    Pushed Hello CMake

---
+++ Hello_Cmake/build/CMakeFiles/Hello_CMake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hello_CMake.dir/main.cpp.o"
+  "CMakeFiles/Hello_CMake.dir/main.cpp.o.d"
+  "Hello_CMake"
+  "Hello_CMake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hello_CMake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
